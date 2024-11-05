34efc2e8044d2a88950567f34078aad3a24163bc
glitt13 <guy.litt@noaa.gov>
2024-11-05 07:10:35 -0700

    fix: address path/glue format issues

--- scripts/config/attr_gen_camels.R
+++ scripts/config/attr_gen_camels.R
@@ -18,12 +18,14 @@ main <- function(){
   if(base::length(cmd_args)!=1){
     warning("Unexpected to have more than one argument in Rscript fs_attrs_grab.R /path/to/attribute_config.yaml.")
   }
-
+  home_dir <- Sys.getenv("HOME")
   # Read in config file, e.g.
-  path_config <- cmd_args[1] # path_config <- "~/git/formulation-selector/scripts/config/attr_gen_camels_config.yaml"
+  path_config <- glue::glue(cmd_args[1]) # path_config <- "~/git/formulation-selector/scripts/config/attr_gen_camels_config.yaml"
   raw_config <- yaml::read_yaml(path_config)
-  dir_std_base <- raw_config$dir_std_base
+
+  dir_std_base <- glue::glue(raw_config$dir_std_base)
   ds_type <- raw_config$ds_type
+  datasets <- raw_config$datasets
   ############################ BEGIN CUSTOM MUNGING ############################
 
   # ----------------------=-- Read in CAMELS gage ids ------------------------ #
@@ -63,12 +65,12 @@ main <- function(){
                                                    featureID='USGS-{gage_id}',
                                                    Retr_Params=Retr_Params,
                                                    overwrite=FALSE)
-  dir_metadata_out <- file.path(Retr_Params$paths$dir_std_base,Retr_Params$datasets)
-  dir.create(dir_metadata_out,recursive = TRUE,showWarnings = FALSE)
+  # dir_metadata_out <- file.path(Retr_Params$paths$dir_std_base,Retr_Params$datasets)
+  # dir.create(dir_metadata_out,recursive = TRUE,showWarnings = FALSE)
   ds <- datasets
-  path_metadata <- file.path(dir_metadata_out,glue::glue( "{dir_std_base}/{ds}/nldi_feat_{ds}_{ds_type}.csv}"))
+  path_metadata <- file.path(glue::glue( "{dir_std_base}/{ds}/nldi_feat_{ds}_{ds_type}.csv"))
   proc.attr.hydfab::write_meta_nldi_feat(dt_site_feat = dt_comids,
-                                         path_meta = path_meta)
+                                         path_meta = path_metadata)
 
   message(glue::glue("Completed attribute acquisition for {Retr_Params$paths$dir_db_attrs}"))
 }
